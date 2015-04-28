5ac82fa549424f92ba506c5a69e44ed4083ac57d
Shiba Prasad Jana <shiba.spj@hotmail.com>
2015-04-28 15:32:07 +0530

    first problem

---
+++ src/in/shiba/multiplesOf3And5.java
@@ -0,0 +1,15 @@
+package in.shiba;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
+ * <p>
+ * Created by shibaprasad on 4/28/2015.
+ */
+public class MultiplesOf3And5 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
